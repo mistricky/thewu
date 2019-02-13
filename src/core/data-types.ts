0f869e48bd2faaa24baf61e33ed4c98f36902f4b
HaoDaWang <1306036576@qq.com>
2019-02-13 23:39:12 +0800

    [New] add data type

---
+++ src/core/data-types.ts
@@ -0,0 +1,8 @@
+export const enum DATA_TYPE {
+  BOOL = "[object Boolean]",
+  NUMBER = "[object Number]",
+  STRING = "[object String]",
+  FUNCTION = "[object Function]",
+  OBJECT = "[object Object]",
+  ARRAY = "[object Array]"
+}
