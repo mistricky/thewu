b0a0d39bb7fd91e34c5fb96d4d9c56a6cfd69eae
HaoDaWang <1306036576@qq.com>
2019-02-13 23:41:52 +0800

    [New] export all modules from core

---
+++ src/core/index.ts
@@ -0,0 +1,7 @@
+export * from "./compiler";
+export * from "./data-types";
+export * from "./decorators";
+export * from "./dom-operation";
+export * from "./element";
+export * from "./h";
+export * from "./renderer";
