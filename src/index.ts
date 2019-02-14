8aa0fb601315ca071c83355500014bdd6d8c694f
HaoDaWang <1306036576@qq.com>
2019-02-15 01:22:28 +0800

    [Update] change declaration path

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+export * from "./core";
+export * from "./robust";
+export * from "./utils";
+
+import "./index";
+
+declare global {
+  namespace JSX {
+    interface Element {}
+
+    // 固有元素的接口
+    interface IntrinsicElements {
+      [index: string]: unknown;
+    }
+
+    // 值类型元素
+    interface ElementClass {
+      render(): Element;
+    }
+
+    interface ElementAttributesProperty {
+      props: unknown;
+    }
+  }
+}
