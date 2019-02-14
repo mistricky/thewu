71755e2a10603af1c5be4396b552aa75d6a9d87e
HaoDaWang <1306036576@qq.com>
2019-02-15 01:25:20 +0800

    [New] add children decorator

--- src/core/decorators/children.ts
+++ src/core/decorators/children.ts
@@ -1,1 +1,9 @@
-export function children() {}
+import "reflect-metadata";
+
+export const CHILDREN_KEY = Symbol("flat:children");
+
+export function Children() {
+  return function(target: any, key: string) {
+    Reflect.defineMetadata(CHILDREN_KEY, key, target);
+  };
+}
