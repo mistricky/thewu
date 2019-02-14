8352961ad36779ae12cbc9490b686886b5376fde
HaoDaWang <1306036576@qq.com>
2019-02-15 01:26:09 +0800

    [New] add props decorator

--- src/core/decorators/prop.ts
+++ src/core/decorators/prop.ts
@@ -1,1 +1,17 @@
-export function prop() {}
+import "reflect-metadata";
+
+export const PROP_KEY = Symbol("flat:prop");
+
+export function Prop() {
+  return function(target: any, key: string) {
+    let originProps: string[] | undefined;
+    let props: string[] = [];
+    props.push(key);
+
+    if ((originProps = Reflect.getMetadata(PROP_KEY, target))) {
+      props = props.concat(originProps);
+    }
+
+    Reflect.defineMetadata(PROP_KEY, props, target);
+  };
+}
