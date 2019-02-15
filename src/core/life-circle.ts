c1709288f0129ca935940da91733b4d409eb07a2
HaoDaWang <1306036576@qq.com>
2019-02-15 22:46:02 +0800

    [Update] change argument type to instance

--- src/core/life-circle.ts
+++ src/core/life-circle.ts
@@ -1,13 +1,16 @@
-import { FlatComponentConstructor, _Element } from "./element";
+import { FlatComponentConstructor, _Element, Component } from "./element";
 
-export function processLifeCircle(
-  component: FlatComponentConstructor
-): _Element {
-  let { componentWillMount, componentDidMount, render } = new component();
+export interface SystemHooks {
+  _sysDidMount(): void;
+  _key: symbol;
+}
 
-  componentWillMount();
-  let vdom = render();
-  componentDidMount();
+/** 只负责调用生命周期钩子 */
+export function processLifeCircle(instance: Component): _Element {
+  instance.componentWillMount();
+  let vdom = instance.render();
+  instance.componentDidMount();
+  instance._sysDidMount();
 
   return vdom;
 }
