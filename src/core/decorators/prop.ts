956ba0c537aeb0f28f38983c1bad4cafa2c0dc9f
HaoDaWang <1306036576@qq.com>
2019-02-15 22:47:35 +0800

    [Update] use markProp

--- src/core/decorators/prop.ts
+++ src/core/decorators/prop.ts
@@ -1,17 +1,10 @@
 import "reflect-metadata";
+import { markProp } from "./@mark-prop";
 
 export const PROP_KEY = Symbol("flat:prop");
 
 export function Prop() {
   return function(target: any, key: string) {
-    let originProps: string[] | undefined;
-    let props: string[] = [];
-    props.push(key);
-
-    if ((originProps = Reflect.getMetadata(PROP_KEY, target))) {
-      props = props.concat(originProps);
-    }
-
-    Reflect.defineMetadata(PROP_KEY, props, target);
+    markProp(target, key, PROP_KEY);
   };
 }
