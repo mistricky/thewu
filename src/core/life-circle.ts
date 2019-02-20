5e6b479d9153be7696e2c07804ef8af11be80913
HaoDaWang <1306036576@qq.com>
2019-02-20 21:37:54 +0800

    [Update] legacy

--- src/core/life-circle.ts
+++ src/core/life-circle.ts
@@ -1,16 +1,23 @@
-import { FlatComponentConstructor, _Element, Component } from "./element";
+// legacy
 
-export interface SystemHooks {
-  _sysDidMount(): void;
-  _key: symbol;
-}
+// import { FlatComponentConstructor, _Element, Component } from "./element";
+// import { Vdom } from "./renderer";
 
-/** 只负责调用生命周期钩子 */
-export function processLifeCircle(instance: Component): _Element {
-  instance.componentWillMount();
-  let vdom = instance.render();
-  instance.componentDidMount();
-  instance._sysDidMount();
+// export interface SystemHooks {
+//   _sysDidMount(): void;
+//   _key: symbol;
+// }
 
-  return vdom;
-}
+// /** 只负责调用生命周期钩子 */
+// export function processLifeCircle(
+//   instance: Component,
+//   execRender: () => Vdom
+// ): _Element {
+//   instance.componentWillMount();
+//   let vdom = instance.render();
+
+//   instance.componentDidMount();
+//   instance._sysDidMount();
+
+//   return vdom;
+// }
