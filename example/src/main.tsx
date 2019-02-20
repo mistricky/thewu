3fe240285f6fa8998d19b03dc1541b9bccb7864a
HaoDaWang <1306036576@qq.com>
2019-02-20 21:38:32 +0800

    [Update] add some test

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -1,21 +1,92 @@
-import Flat, { FlatComponent, Prop, _Element, Ele, Children } from "../../dist";
+import Flat, {
+  FlatComponent,
+  Prop,
+  _Element,
+  FlatElement,
+  Children,
+  State
+} from "../../dist";
 
-class Greeter extends FlatComponent {
+interface GreeterProps {
+  children: string[];
+  a: string;
+}
+
+interface PersonProps {
+  name: string;
+  age: number;
+}
+
+class Person extends FlatComponent<PersonProps> {
+  @Prop()
+  name!: string;
+
+  @Prop()
+  age!: number;
+
+  render() {
+    console.info("person render");
+    console.info(this.name, this.age);
+
+    return (
+      <div>
+        <p>name: {this.name}</p>
+        <p>age: {this.age}</p>
+      </div>
+    );
+  }
+}
+
+class Greeter extends FlatComponent<GreeterProps> {
+  constructor() {
+    super();
+    // setTimeout(() => {
+    //   this.age = 20;
+    // }, 1000);
+  }
+
   @Children()
   names!: string[];
 
+  @State()
+  age = 19;
+
+  @Prop()
+  a!: string;
+
+  componentWillMount() {
+    console.info("will mount");
+  }
+
+  componentDidMount() {
+    console.info("did mount");
+  }
+
   render(): JSX.Element {
     let greeters = names.map(name => <p>hello {name}</p>);
+    console.info("greeter render");
+    console.info(this.a);
 
-    return <div>{greeters}</div>;
+    return (
+      <div>
+        <p>{greeters}</p>
+        <Person name="zhangsan" age={10} />
+      </div>
+    );
   }
+}
+
+function hello() {
+  console.info(hello);
 }
 
 let names = ["A", "B", "C"];
 let input = (
-  <div id="foo">
-    <Greeter>{names}</Greeter>
+  <div id="foo" onclick={hello}>
+    <Greeter a="aa">{names}</Greeter>
   </div>
 );
 
-new Ele(input).bindDOM(document.querySelector("#root"));
+new FlatElement(Object.assign({}, input)).bindDOM(
+  document.querySelector("#root")
+);
