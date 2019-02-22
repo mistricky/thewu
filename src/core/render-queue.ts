5e1e120f8fed4e87eb01070d67abd0a2b26c6a68
HaoDaWang <1306036576@qq.com>
2019-02-22 23:59:56 +0800

    [Update] add deferAttrs

--- src/core/render-queue.ts
+++ src/core/render-queue.ts
@@ -1,12 +1,28 @@
+import { Attrs } from "./element";
+import { mapPop } from "../utils";
+
 export class RenderQueue {
   // render queue
-  keys: Symbol[] = [];
+  private _keys: Symbol[] = [];
+  private deferAttrs: Map<Symbol, Attrs> = new Map();
+
+  get keys() {
+    return this._keys;
+  }
 
   removeKey(_key: Symbol) {
-    this.keys.splice(this.keys.indexOf(_key), 1);
+    this._keys.splice(this._keys.indexOf(_key), 1);
   }
 
   addKey(_key: Symbol) {
-    this.keys.push(_key);
+    this._keys.push(_key);
+  }
+
+  getAttrs(_key: Symbol): Attrs | undefined {
+    return mapPop(this.deferAttrs, _key);
+  }
+
+  setAttrs(_key: Symbol, attrs: Attrs) {
+    this.deferAttrs.set(_key, attrs);
   }
 }
