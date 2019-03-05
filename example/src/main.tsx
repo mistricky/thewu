64637ecd17c9a38f82c4f89fcb9f63cf656c3a66
HaoDaWang <1306036576@qq.com>
2019-03-05 20:33:43 +0800

    [Update] test

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -1,4 +1,4 @@
-import Flat, { _Element, FlatElement, FlatComponent } from "../../dist";
+import Flat, { _Element, FlatElement, FlatComponent, State } from "../../dist";
 import { App } from "./views/app";
 
 new FlatElement(<App />).bindDOM(document.querySelector("#root"));
