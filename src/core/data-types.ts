c3478f83562f3693cc3787d30e26f9d45e89ad18
HaoDaWang <1306036576@qq.com>
2019-03-12 13:39:50 +0800

    [format] update to single quote

--- src/core/data-types.ts
+++ src/core/data-types.ts
@@ -1,8 +1,9 @@
 export const enum DATA_TYPE {
-  BOOL = "[object Boolean]",
-  NUMBER = "[object Number]",
-  STRING = "[object String]",
-  FUNCTION = "[object Function]",
-  OBJECT = "[object Object]",
-  ARRAY = "[object Array]"
+  BOOL = '[object Boolean]',
+  NUMBER = '[object Number]',
+  STRING = '[object String]',
+  FUNCTION = '[object Function]',
+  OBJECT = '[object Object]',
+  ARRAY = '[object Array]',
+  INPUT_ELEMENT = '[object HTMLInputElement]'
 }
