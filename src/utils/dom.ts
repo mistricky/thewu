0a10601b82355e81f5e5b53c5629eac05e998fe6
HaoDaWang <1306036576@qq.com>
2019-03-21 23:15:51 +0800

    [New] add dom util

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,11 +1,66 @@
 import { ROOT_ELEMENT_ID } from '../core/constant';
+import { VdomNode } from '../core';
 
-export function replace(originEle: HTMLElement, targetEle: HTMLElement) {
-  let parent = originEle.parentElement;
+function getParentNode(dom: HTMLElement): HTMLElement {
+  let parent = dom.parentElement;
 
   if (!parent) {
     parent = document.getElementById(ROOT_ELEMENT_ID)!;
   }
 
+  return parent;
+}
+
+export function replace(originEle: HTMLElement, targetEle: HTMLElement) {
+  let parent = getParentNode(originEle);
+
   parent.replaceChild(targetEle, originEle);
+}
+
+export function add(
+  originEle: HTMLElement,
+  targetIndex: number,
+  targets: VdomNode[]
+) {
+  let parent = getParentNode(originEle);
+  let child = parent.children[targetIndex + 1];
+
+  for (let target of targets) {
+    let targetEl = target.el;
+
+    if (!targetEl) {
+      continue;
+    }
+
+    parent.insertBefore(targetEl, child);
+  }
+}
+
+export function remove(originEle: HTMLElement, targetIndex: number[]) {
+  let parent = getParentNode(originEle);
+  let children = parent.children;
+  let stashChildren = [];
+
+  // 为了防止下标变换
+  for (let index of targetIndex) {
+    stashChildren.push(children[index]);
+  }
+
+  for (let child of stashChildren) {
+    parent.removeChild(child);
+  }
+}
+
+export function move(
+  originEle: HTMLElement,
+  originIndex: number,
+  targetIndex: number
+) {
+  let parent = getParentNode(originEle);
+  let children = parent.children;
+  let originChild = children[originIndex];
+  let targetChild = children[targetIndex];
+
+  parent.insertBefore(originChild, targetChild);
+  parent.removeChild(targetChild);
 }
