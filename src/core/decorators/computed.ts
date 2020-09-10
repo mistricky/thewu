cb0f2a6de78b4b04bf80586dfbfaecc9c7f57773
youncccat <1306036576@qq.com>
2020-09-10 23:38:21 +0800

    [Update] make getter enumerable

--- src/core/decorators/computed.ts
+++ src/core/decorators/computed.ts
@@ -1,8 +1,8 @@
 export const COMPUTED_KEY = Symbol('flat:computed');
 
 export function Computed() {
-  return function(target: any, key: string) {
-    console.info(target);
+  return function(target: any, key: string, descriptor: PropertyDescriptor) {
+    descriptor.enumerable = true;
     // markProp(target, key, STATE_KEY);
     Reflect.defineMetadata(COMPUTED_KEY, `${key}|${Date.now()}`, target, key);
   };
