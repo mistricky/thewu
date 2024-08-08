6e3735c3f9f37bf2d409903754c0280d0959cd86
Mist <mist.zzh@gmail.com>
2024-08-08 18:45:55 +0800

    [Feat] add publish script

---
+++ scripts/publish.ts
@@ -0,0 +1,26 @@
+import { join } from "jsr:@std/path";
+
+const publish = async (path: string) => {
+  const command = new Deno.Command("pnpm", {
+    cwd: path,
+    args: ["publish", "--access=public"],
+  });
+  const { stdout, stderr } = await command.output();
+  const decoder = new TextDecoder();
+
+  if (stderr.length) {
+    console.log(decoder.decode(stderr));
+    Deno.exit(1);
+  }
+
+  console.log(decoder.decode(stdout));
+};
+
+const packagesPath = join("..", "packages");
+
+// Sync version to all workspace packages
+for await (const pkg of Deno.readDir(packagesPath)) {
+  const pkgPath = join(packagesPath, pkg.name);
+
+  await publish(pkgPath);
+}
